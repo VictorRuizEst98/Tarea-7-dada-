71ec4ddb84af6828dbfe2b3e21d5d1e06b039525
VictorRuizEst98 <31596271+VictorRuizEst98@users.noreply.github.com>
2019-08-27 23:29:22 -0500

    Add files via upload

---
+++ Tarea 7 dada.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Calculadora")
+print("Sumas")
+
+
+# In[2]:
+
+
+n1 = int(input("introduce el primer numero: "))
+
+
+# In[3]:
+
+
+n2 = int(input("introduce el segundo numero: "))
+
+
+# In[4]:
+
+
+suma = n1 + n2
+
+
+# In[5]:
+
+
+print("Esta es la suma: ")
+print(suma)
+
+
+# In[6]:
+
+
+print("Restas")
+
+
+# In[7]:
+
+
+n1 = int(input("introduce el primer numero numero: "))
+
+
+# In[8]:
+
+
+n2 = int(input("introduce el segundo numero: "))
+
+
+# In[9]:
+
+
+resta = n1 - n2
+
+
+# In[10]:
+
+
+print(resta)
+
+
+# In[11]:
+
+
+print("Multiplicaciones")
+
+
+# In[12]:
+
+
+n1 = int(input("introduce el primer numero: "))
+
+
+# In[13]:
+
+
+n2 = int(input("introduce el segundo numero: "))
+
+
+# In[14]:
+
+
+multiplicacion = n1 * n2
+
+
+# In[15]:
+
+
+print(multiplicacion)
+
+
+# In[16]:
+
+
+print("Divisiones")
+
+
+# In[17]:
+
+
+n1 = int(input("introduce un numero: "))
+
+
+# In[18]:
+
+
+n2 = int(input("introduce un numero: "))
+
+
+# In[19]:
+
+
+division = n1 / n2
+
+
+# In[20]:
+
+
+print(division)
+
